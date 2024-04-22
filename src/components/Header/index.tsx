d7ed4cea3379acf34ac4bc1b2d525298759be168
Mohamed Shebl <hamdysheblm@gmail.com>
2024-04-22 21:30:40 +0200

    + update words spelling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -92,10 +92,10 @@ export default function Header() {
             Home
           </NavLink>
           <NavLink
-            to="/favourite"
+            to="/favorite"
             className="text-sm font-semibold leading-6 text-gray-900 hover:text-blue-700"
           >
-            Favourite
+            Favorite
           </NavLink>
 
           <Popover className="relative">
@@ -202,10 +202,10 @@ export default function Header() {
                   Home
                 </NavLink>
                 <NavLink
-                  to="/favourite"
+                  to="/favorite"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 hover:text-blue-700"
                 >
-                  Favourite
+                  Favorite
                 </NavLink>
 
                 <Disclosure as="div" className="-mx-3">
